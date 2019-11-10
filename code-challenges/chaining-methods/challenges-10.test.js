a22d0ce6fc43d19e7f5adef4e3fcff41df52bbe1
David Vloedman <djvloedman@gmail.com>
2019-11-10 15:29:21 -0800

    Test three

--- code-challenges/chaining-methods/challenges-10.test.js
+++ code-challenges/chaining-methods/challenges-10.test.js
@@ -38,9 +38,10 @@ This function should then raise 2 to the power of the resulting numbers, returni
 For example, [ [0,2,5,4], [2,4,10], [] ] should return [ [1, 32], [1024], [] ].
 ------------------------------------------------------------------------------------------------ */
 
-const divisibleByFiveTwoToThePower = input => {
-  // Solution code here...
-};
+const divisibleByFiveTwoToThePower = input =>
+  input
+    .map(a => a.filter(b => typeof b === 'number' && b % 5 === 0))
+    .map(c => c.map(d => Math.pow(2, d)));
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
